c8902e8d653be966e87971e81a769e23e44ea98f
futurealok1445 <futuregameozofficial@gmail.com>
2026-03-12 00:23:42 +0530

    Update elastic search to use cloud build

--- todo/src/elastic.ts
+++ todo/src/elastic.ts
@@ -3,10 +3,6 @@ import { Client } from "@elastic/elasticsearch"
 export const esClient = new Client({
   node: process.env.ELASTIC_NODE || "https://localhost:9200",
   auth: {
-    username: "elastic",
-    password: process.env.ELASTIC_PASSWORD || "YOUR_PASSWORD"
+    apiKey: process.env.ELASTIC_PASSWORD || "YOUR_PASSWORD"
   },
-  tls: {
-    rejectUnauthorized: false
-  }
 })
